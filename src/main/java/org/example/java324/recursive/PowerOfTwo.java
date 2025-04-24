8c5b503861f4f1205dcbedf853d254556b9f96ab
ginny169 <tongdong13@gmail.com>
2025-04-24 14:48:47 +0700

    recursive

--- src/main/java/org/example/java324/recursive/PowerOfTwo.java
+++ src/main/java/org/example/java324/recursive/PowerOfTwo.java
@@ -1,14 +1,26 @@
 package org.example.java324.recursive;
 
 public class PowerOfTwo {
-    public static boolean isPowerOfTwo(int n) {
-        if (n == 1) return true;
-        if (n <= 0) return false;
-        if (n % 2 != 0) return false;
-        return isPowerOfTwo(n / 2); //đệ quy n/2 đến tận n = 1;
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n < 0){
+            return 1/myPow(x,-n);
+        }
+        //2*16 = 2^8 * 2^8
+        //2^15 = 2* 2^7*2^7
+        if (n%2!=0){
+            return x * myPow(x, n/2) * myPow(x, n/2);
+        } else {
+            return myPow(x, n/2)*myPow(x, n/2);
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(2));
+        System.out.println(myPow(2, 3));
     }
 }
